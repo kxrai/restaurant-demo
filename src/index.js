17fbcf3540ffdb23cf604b6a661a721f44927f17
kxrai <alicia.2005.loi@gmail.com>
2023-09-28 15:11:19 -0400

    started again

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// the file where we connect our react application to the dom
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import React from "react";
-import { ReactDOM } from "react";
-
+import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
