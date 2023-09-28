f119107529559b7c0d83ced1468fd47c447d4619
kxrai <alicia.2005.loi@gmail.com>
2023-09-28 13:00:08 -0400

    changed stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//importing components and containers
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// the file where we connect our react application to the dom
 
-import './index.css';
+import React from "react";
+import { ReactDOM } from "react";
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
