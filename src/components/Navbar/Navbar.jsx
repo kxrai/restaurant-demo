b27f7d275ed278f33c4ef1b1744f2147051c5c1d
kxrai <alicia.2005.loi@gmail.com>
2023-09-29 10:41:12 -0400

    working on nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+// importing icons..can prob change inmage after
+import { GiHamburgerMenu} from 'react-icons/gi';
+import { MdOutlinesRestaurantMenu} from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
+  <nav className="app__navbar">
     Navbar
-  </div>
+  </nav>
 );
 
 export default Navbar;
